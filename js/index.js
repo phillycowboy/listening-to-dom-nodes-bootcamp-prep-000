ebbab9c4685edb84dd2125da96e41a6667df37b9
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-18 17:41:43 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -36,4 +36,7 @@ divs = document.querySelectorAll('div');
  
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
-}for (let i = 0; i < divs.length; i++) {
+}for (let i = 0; i < divs.length; i++) { 
+  divs[i].addEventListener('click', capture, true);
+}
+
