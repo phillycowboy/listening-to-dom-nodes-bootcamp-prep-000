ce1de25d3a1929200c3e10931ddcf99781cb2bdf
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-18 17:41:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -32,3 +32,8 @@ for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 }
 
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
+}
