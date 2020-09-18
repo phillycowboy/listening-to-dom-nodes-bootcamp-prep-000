c26aa843d267b2c9cf5e1263fa729cd622c9173b
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-18 17:40:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -25,4 +25,10 @@ input.addEventListener('keydown', function(e) {
 let divs = document.querySelectorAll('div');
 
 function bubble(e) {
-  
+   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
+
